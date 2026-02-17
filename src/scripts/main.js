b7eeafdfe6d603c4d3e8b9a1709907cc44915c13
Natalia Humenyk <gumenuknatalia53@gmail.com>
2026-02-17 22:00:18 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+function getSalary(element) {
+  return Number(element.dataset.salary);
+}
+
+function sortList(workerList) {
+  const items = Array.from(workerList.children);
+
+  items.sort((a, b) => getSalary(b) - getSalary(a));
+
+  workerList.append(...items);
+
+  return items;
+}
+
+function getEmployees(workerList) {
+  const items = Array.from(workerList.children);
+
+  return items.map((item) => ({
+    name: (item.dataset.name || '').trim(),
+    position: (item.dataset.position || '').trim(),
+    salary: getSalary(item) || 0,
+    age: Number(item.dataset.age) || 0,
+  }));
+}
+export { sortList, getEmployees };
